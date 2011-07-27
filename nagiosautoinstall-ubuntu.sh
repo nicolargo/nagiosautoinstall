eacc40dd55a47a0f534c9000bce04ee4d86602ff
nicolargo <nicolargo@pcdesktopboulot.alcasat.net>
2011-07-27 15:22:25 +0200

    + libperl-dev

--- nagiosautoinstall-ubuntu.sh
+++ nagiosautoinstall-ubuntu.sh
@@ -6,7 +6,7 @@
 #
 # Syntaxe: # sudo ./nagiosautoinstall-ubuntu.sh
 #
-version="0.8"
+version="0.81"
 
 nagios_core_version="3"
 nagios_core_subversion="3.3.1"
@@ -22,7 +22,7 @@ installation() {
   echo "----------------------------------------------------"
   echo "Installation de pre-requis / Configuration Postfix"
   echo "----------------------------------------------------"
-  $apt install apache2 wget libapache2-mod-php5 build-essential libgd2-xpm-dev 
+  $apt install apache2 wget libapache2-mod-php5 build-essential libgd2-xpm-dev libperl-dev
   $apt install bind9-host dnsutils libbind9-60 libdns66 libisc60 libisccc60 libisccfg60 liblwres60 libradius1 qstat radiusclient1 snmp snmpd
   $apt install libgd2-noxpm-dev libpng12-dev libjpeg62 libjpeg62-dev
   $apt install fping libnet-snmp-perl libldap-dev libmysqlclient-dev libgnutls-dev libradiusclient-ng-dev
