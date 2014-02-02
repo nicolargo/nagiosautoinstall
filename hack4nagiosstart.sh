6f538f919eb0537a07b7ed771f158e70c8ba621d
Nicolas Hennion <nicolas@nicolargo.com>
2014-02-02 10:09:02 +0100

    Update hack script

--- hack4nagiosstart.sh
+++ hack4nagiosstart.sh
@@ -6,7 +6,7 @@
 # /etc/init.d/nagios start
 # /etc/init.d/nagios: 20: .: Can't open /etc/rc.d/init.d/functions
 # 
-# Nicolargo - 2013 - GPL
+# Nicolargo - 2014 - GPL
 
 sudo apt-get install daemon
 if [ ! -e /etc/rc.d/init.d/functions ]; 
@@ -17,7 +17,10 @@ if [ ! -e /etc/rc.d/init.d/functions ];
         sudo sed -i 's/\/var\/lock\/subsys\/\$prog/\/var\/lock\/\$prog/g' /etc/init.d/nagios
         sudo sed -i 's/\/sbin\/service\ nagios\ configtest/\/usr\/sbin\/service\ nagios\ configtest/g' /etc/init.d/nagios
         sudo sed -i 's/\"\ \=\=\ \"/\"\ \=\ \"/g' /etc/init.d/nagios
-        sudo sed -i 's/\#\#killproc\ \-p\ \$\{pidfile\}\ \-d\ 10/killproc\ \-p \$\{pidfile\}/g' /etc/init.d/nagios
-        sudo sed -i 's/runuser/su/g' /etc/init.d/nagios
+        sudo sed -i "s/\#\#killproc\ \-p\ \${pidfile\}\ \-d\ 10/killproc\ \-p \${pidfile\}/g" /etc/init.d/nagios
+        sudo sed -i "s/runuser/su/g" /etc/init.d/nagios
+        sudo sed -i "s/use_precached_objects=\"false\"/&\ndaemonpid=\$(pidof daemon)/" /etc/init.d/nagios
+        sudo sed -i "s/killproc\ -p\ \${pidfile}\ -d\ 10\ \$exec/\/sbin\/start-stop-daemon\ --user=\$user\ \$exec\ --stop/g" /etc/init.d/nagios
+        sudo sed -i "s/\/sbin\/start-stop-daemon\ --user=\$user\ \$exec\ --stop/&\n\tkill -9 \$daemonpid/" /etc/init.d/nagios
     fi
 sudo service nagios start
